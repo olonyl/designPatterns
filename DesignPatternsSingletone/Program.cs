8a894f3c1e76abce99475b3849474b10116aad5d
Olonyl Landeros <jose87.landeros@gmail.com>
2023-09-12 09:45:35 -0600

    Thread safety

--- DesignPatternsSingletone/Program.cs
+++ DesignPatternsSingletone/Program.cs
@@ -1,19 +1,48 @@
 ﻿using DesignPatternsSingletone.BasicImplementation;
+using System;
 using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
 using static System.Console;
 
 namespace DesignPatternsSingletone
 {
+    public sealed class PerThreadSingleton
+    {
+        private static ThreadLocal<PerThreadSingleton> threadInstance = 
+            new ThreadLocal<PerThreadSingleton>(
+                () => new PerThreadSingleton());
+
+        public int Id;
+        private PerThreadSingleton()
+        {
+            Id = Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public static PerThreadSingleton Instance = threadInstance.Value;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            var db = SingletonDatabase.Instance;
-            var db2 = SingletonDatabase.Instance;
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("t1: " + PerThreadSingleton.Instance.Id);
+            });
 
-            var city = "Mexico City";
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+                Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+            });
 
-            WriteLine($"City {city} has population {db.GetPopulation(city)}");
+            Task.WaitAll(t1,t2);
+            //var db = SingletonDatabase.Instance;
+            //var db2 = SingletonDatabase.Instance;
+
+            //var city = "Mexico City";
+
+            //WriteLine($"City {city} has population {db.GetPopulation(city)}");
         }
     }
 }
