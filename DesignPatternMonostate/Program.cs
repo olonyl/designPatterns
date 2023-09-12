75f29ec5662e45cbd2db10daecf3b769bad3ac49
Olonyl Landeros <jose87.landeros@gmail.com>
2023-09-12 08:35:28 -0600

    Added Monostate example, can Instantiate a class as many times as possible , however the members will always contain the same values

---
+++ DesignPatternMonostate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DesignPatternMonostate
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}:{Age}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new CEO();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new CEO();
+
+            Console.WriteLine(ceo2);
+        }
+    }
+}
