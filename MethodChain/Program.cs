7aec6fc17375f48ab3dda295bc6785830690745d
Olonyl Landeros <jose87.landeros@gmail.com>
2023-10-16 17:43:57 -0600

    Added Chain Of Responsabilities -> Method Chain

---
+++ MethodChain/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace MethodChain
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attack, Defense;
+
+        public Creature(string name, int attack, int defense)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
+        }
+    }
+
+    public class CreatureModifier
+    {
+        protected Creature creature;
+        protected CreatureModifier next;
+
+        public CreatureModifier(Creature creature)
+        {
+            this.creature = creature;
+        }
+
+        public void Add(CreatureModifier cm)
+        {
+            if (next != null) next.Add(cm);
+            else next = cm;
+        }
+
+        public virtual void Handle() => next?.Handle();
+
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Creature creature) : base(creature) { }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}'s attack");
+            creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    public class IncreasedDefenseModifier : CreatureModifier
+    {
+        public IncreasedDefenseModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Increasing {creature.Name}'s defense");
+            creature.Defense += 3;
+            //base.Handle();
+        }
+    }
+
+    public class NoBonusesModifier : CreatureModifier
+    {
+        public NoBonusesModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var goblin = new Creature("Goblin", 2, 2);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+            root.Add(new NoBonusesModifier(goblin));
+
+            Console.WriteLine("Let's double the goblin's attack");
+            root.Add(new DoubleAttackModifier(goblin));
+
+            Console.WriteLine("Let's increase goblin's defense");
+            root.Add(new IncreasedDefenseModifier(goblin));
+            
+            root.Handle();
+
+            Console.WriteLine(goblin);
+        }
+    }
+}
