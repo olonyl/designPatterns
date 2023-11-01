b013bdc722c10325464fd61bd9779c9b159ed139
Olonyl Landeros <jose87.landeros@gmail.com>
2023-11-01 08:09:03 -0600

    Added Memento pattern

---
+++ Memento.DesignPattern/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace Memento.DesignPattern
+{
+    public class Memento
+    {
+        public int Balance { get; }
+        public Memento(int balance)
+        {
+            Balance = balance;
+        }
+    }
+    public class BankAccount
+    {
+        int balance;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+        }
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            return new Memento(balance);
+        }
+
+        public void Restore(Memento memento)
+        {
+            balance = memento.Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)} : {balance}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount(100);
+            var m1 = ba.Deposit(50);
+            var m2 = ba.Deposit(25);
+
+            WriteLine(ba);
+
+            ba.Restore(m1);
+            WriteLine(ba);
+
+            ba.Restore(m2);
+            WriteLine(ba);
+        }
+    }
+}
