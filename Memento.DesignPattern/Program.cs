090cbb07b5b3d5397e5df8281deb03f205fa47ea
Olonyl Landeros <jose87.landeros@gmail.com>
2023-11-03 09:44:21 -0600

    Added Token/Memento excercise

--- Memento.DesignPattern/Program.cs
+++ Memento.DesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using static System.Console;
 
@@ -15,20 +16,54 @@ namespace Memento.DesignPattern
     public class BankAccount
     {
         int balance;
-
+        private List<Memento> changes = new List<Memento>();
+        private int current;
         public BankAccount(int balance)
         {
             this.balance = balance;
+            changes.Add(new Memento(balance));
         }
         public Memento Deposit(int amount)
         {
             balance += amount;
-            return new Memento(balance);
+            var m = new Memento(balance);
+            changes.Add(m);
+            current++;
+
+            return m;
         }
 
-        public void Restore(Memento memento)
+        public Memento Restore(Memento memento)
         {
-            balance = memento.Balance;
+            if( memento== null )
+            {
+                balance = memento.Balance;
+                changes.Add(memento);
+                return memento;
+            }
+            return null;
+        }
+
+        public Memento Undo()
+        {
+            if(current > 0)
+            {
+                var m = changes[--current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
+        }
+
+        public Memento Redo()
+        {
+            if (current + 1 < changes.Count)
+            {
+                var m = changes[++current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
         }
 
         public override string ToString()
@@ -41,16 +76,34 @@ namespace Memento.DesignPattern
         static void Main(string[] args)
         {
             var ba = new BankAccount(100);
-            var m1 = ba.Deposit(50);
-            var m2 = ba.Deposit(25);
-
+            ba.Deposit(50);
+            ba.Deposit(25);
             WriteLine(ba);
 
-            ba.Restore(m1);
+            ba.Undo();
+            WriteLine($"Undo 1: {ba}");
+
+            ba.Undo();
+            WriteLine($"Undo 2: {ba}");
+
+            ba.Redo();
+            WriteLine($"Redo 1: {ba}");
+
+            ba.Deposit(80);
             WriteLine(ba);
 
-            ba.Restore(m2);
+            ba.Undo();
+            WriteLine($"Undo 3: {ba}");
+
+            ba.Redo();
+            WriteLine($"Undo 2: {ba}");
+
+            ba.Undo();
+            WriteLine($"Redo 3: {ba}");
+
+            ba.Deposit(200);
             WriteLine(ba);
+
         }
     }
 }
---
+++ MementoExcercise.DesignPattern/Program.cs
@@ -0,0 +1,71 @@
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MementoExcercise.DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tMachine = new TokenMachine();
+            var token = new Token(111);
+
+            var memento = tMachine.AddToken(token);
+            token.Value = 333;
+
+            var token2 = new Token(222);
+            var memento3 = tMachine.AddToken(token2);
+
+            var token3 = new Token(100);
+            var memento4 = tMachine.AddToken(token3);
+
+            tMachine.Revert(memento3);
+
+            Console.WriteLine(tMachine.Tokens.Count);
+        }
+    }
+}
+
+namespace Coding.Exercise
+{
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        public List<Token> Tokens = new List<Token>();
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            return AddToken(new Token(value));
+        }
+
+        public Memento AddToken(Token token)
+        {
+            Tokens.Add(token);
+            var m = new Memento();
+            // a rather roundabout way of cloning
+            m.Tokens = Tokens.Select(t => new Token(t.Value)).ToList();
+            return m;
+        }
+
+        public void Revert(Memento m)
+        {
+            Tokens = m.Tokens.Select(mm => new Token(mm.Value)).ToList();
+        }
+    }
+}
