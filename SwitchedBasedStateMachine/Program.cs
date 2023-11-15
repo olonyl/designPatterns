dde6b89393031542e13847a9a2ee1453df1797ac
Olonyl Landeros <jose87.landeros@gmail.com>
2023-11-15 14:33:12 -0600

    Transition state using SwitchedBased State machine

---
+++ SwitchedBasedStateMachine/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SwitchedBasedStateMachine
+{
+    enum State
+    {
+        Locked, 
+        Failed,
+        Unlocked
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string code = "1234";
+            var state = State.Locked;
+            var entry = new StringBuilder();
+
+            while (true)
+            {
+                switch (state)
+                {
+                    case State.Locked:
+                        entry.Append(Console.ReadKey().KeyChar);
+                        if(entry.ToString() == code)
+                        {
+                            state = State.Unlocked;
+                            break;
+                        }
+                        if (!code.StartsWith(entry.ToString()))
+                        {
+                            state= State.Failed;
+                        }
+                        break;
+                    case State.Failed:
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("FAILED");
+                        entry.Clear();
+                        state = State.Locked;
+                        break;
+                    case State.Unlocked:
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("UNLOCKED");
+                        return;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
