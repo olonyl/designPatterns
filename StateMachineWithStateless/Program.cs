0c84e60112c6afdbbbe37eb8a0b517eafb776049
Olonyl Landeros <jose87.landeros@gmail.com>
2023-11-15 15:43:00 -0600

    State machine using Stateless library

---
+++ StateMachineWithStateless/Program.cs
@@ -0,0 +1,43 @@
+﻿using Stateless;
+using System;
+
+namespace StateMachineWithStateless
+{
+    public enum Health
+    {
+        NonReproductive, 
+        Pregnant, 
+        Reproductive
+    }
+
+    public enum Activity
+    {
+        GiveBirth,
+        ReachPuberty, 
+        HaveAbortion,
+        HaveUnprotectedSex,
+        Historectomy
+    }
+
+    internal class Program
+    {
+        public static bool ParentsNotWatching { get; private set; }
+
+        static void Main(string[] args)
+        {
+           var machine = new StateMachine<Health, Activity>(Health.NonReproductive);
+            machine.Configure(Health.NonReproductive)
+                .Permit(Activity.ReachPuberty, Health.Reproductive);
+
+            machine.Configure(Health.Reproductive)
+                .Permit(Activity.Historectomy, Health.NonReproductive)
+                .PermitIf(Activity.HaveUnprotectedSex, Health.Pregnant, () => ParentsNotWatching);
+
+            machine.Configure(Health.Pregnant)
+                .Permit(Activity.GiveBirth, Health.Reproductive)
+                .Permit(Activity.HaveAbortion, Health.Reproductive);
+
+
+        }
+    }
+}
