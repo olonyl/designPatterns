324d28e4f8b0b180bedd327e6d2febe334c22d39
Olonyl Landeros <jose87.landeros@gmail.com>
2023-11-15 14:47:54 -0600

    Switch expression to build a State Machine

---
+++ SwitchExpression/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SwitchExpression
+{
+    enum Chest
+    {
+        Open, 
+        Closed,
+        Locked
+    }
+    enum Action
+    {
+        Open, 
+        Close
+    }
+
+    internal class Program
+    {
+        static Chest Manipulate
+            (Chest chest, Action action, bool haveKey) =>
+            (chest, action, haveKey) switch
+            {
+                (Chest.Locked, Action.Open, true) => Chest.Open,
+                (Chest.Closed, Action.Open, _) => Chest.Open,
+                (Chest.Open, Action.Close, true) => Chest.Locked,
+                (Chest.Open, Action.Close, _) => Chest.Closed,
+
+                _ => chest
+            };
+        static void Main(string[] args)
+        {
+            var chest = Chest.Locked;
+            Console.WriteLine($"Chest is {chest}");
+
+            chest = Manipulate(chest, Action.Open, true);
+            Console.WriteLine($"Chest is {chest}");
+
+            chest = Manipulate(chest, Action.Close, false);
+            Console.WriteLine($"Chest is {chest}");
+
+            chest = Manipulate(chest, Action.Close, false);
+            Console.WriteLine($"Chest is {chest}");
+        }
+    }
+}
