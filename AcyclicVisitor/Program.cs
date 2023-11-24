74dab8d46d7cdeb73facd7c9bf1f37617a17ca07
Olonyl Landeros <jose87.landeros@gmail.com>
2023-11-24 15:46:29 -0600

    Added Acyclic Visitor

---
+++ AcyclicVisitor/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+namespace AcyclicVisitor
+{
+    public interface IVisitor<TVisitable>
+    {
+        void Visit(TVisitable obj);   
+    }
+
+    public interface IVisitor { }
+
+    public abstract class Expression
+    {
+       public virtual void Accept(IVisitor visitor)
+        {
+            if(visitor is IVisitor<Expression> typed)
+                typed.Visit(this);
+        }
+    }
+    public class DoubleExpression : Expression
+    {
+        public double Value;
+        public DoubleExpression(double value)
+        {
+            this.Value = value;
+        }
+        public override void Accept(IVisitor visitor)
+        {
+            if (visitor is IVisitor<DoubleExpression> typed)
+                typed.Visit(this);
+        }
+    }
+
+    public class AdditionExpression : Expression
+    {
+        public Expression Left, Right;
+        public AdditionExpression(Expression left, Expression right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public override void Accept(IVisitor visitor)
+        {
+            if (visitor is IVisitor<AdditionExpression> typed)
+                typed.Visit(this);
+        }
+    }
+
+    public class ExpressionPrinter : IVisitor
+        , IVisitor<Expression>
+        , IVisitor<DoubleExpression>
+        , IVisitor<AdditionExpression>
+    {
+        private StringBuilder sb = new StringBuilder();
+        public void Visit(AdditionExpression obj)
+        {
+            sb.Append("(");
+            obj.Left.Accept(this);
+            sb.Append("+");
+            obj.Right.Accept(this);
+            sb.Append(")");
+        }
+
+        public void Visit(DoubleExpression obj)
+        {
+            sb.Append(obj.Value);
+        }
+
+        public void Visit(Expression obj) { }
+
+        public override string ToString() => sb.ToString();
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = new AdditionExpression(
+            new DoubleExpression(1),
+            new AdditionExpression(new DoubleExpression(2),
+                                  new DoubleExpression(3)));
+
+            var ep = new ExpressionPrinter();
+            ep.Visit(e);
+            Console.WriteLine(ep);
+        }
+    }
+}
